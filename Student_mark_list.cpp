bc793e2553ab6979b2df20fd20538b1de553f6da
Naveenkumar-230 <your-email@gmail.com>
2025-12-19 15:08:06 +0530

    Student_mark_list created

--- Student_mark_list.cpp
+++ Student_mark_list.cpp
@@ -1,1 +1,25 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string Studentname;
+    int age,Tamil,English,Maths,Science,Social,Total;
+    cout << "Student name:";
+    getline (cin, Studentname);
+    cout << "age";
+    cin >> age;
+    cout << "Tamil:";
+    cin >>Tamil;
+    cout << "English:";
+    cin >> English;
+    cout << "Maths:";
+    cin >> Maths;
+    cout << "Science:";
+    cin >> Science;
+    cout << "Social:";
+    cin >> Social;
+    Total = Tamil+English+Maths+Science+Social;
+    cout << "Total mark:"<< Total;
+    return 0;
+}
